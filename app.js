a9900725ce97ce07198c1866bf6245e23ee1d049
elie kitadi <Kitadi Elie>
2021-09-24 07:59:34 +0300

    project finished

---
+++ app.js
@@ -0,0 +1,89 @@
+const wrapper = document.querySelector(".wrapper"),
+  inputPart = wrapper.querySelector(".input-part"),
+  infoTxt = inputPart.querySelector(".info-txt"),
+  inputField = inputPart.querySelector("input"),
+  apiKey = "e4fd81f20b9613b1faac835b7f40666b",
+  locationBtn = inputPart.querySelector("button"),
+  wIcon = document.querySelector(".weather-part img"),
+  arrowBack = wrapper.querySelector("header i");
+
+let api;
+
+inputField.addEventListener("keyup", (e) => {
+  if (e.key == "Enter" && inputField.value != "") {
+    requestApi(inputField.value);
+  }
+});
+
+locationBtn.addEventListener("click", () => {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(onSuccess, onError);
+  } else {
+    alert("Your browser not support geolocalisation api");
+  }
+});
+
+function onSuccess(position) {
+  const { latitude, longitude } = position.coords;
+
+  api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${apiKey}`;
+  fetchData(api);
+}
+
+function onError(error) {
+  infoTxt.innerText = error.message;
+  infoTxt.classList.replace("pending", "error");
+}
+
+function requestApi(city) {
+  api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+  fetchData(api);
+}
+
+function fetchData(api) {
+  infoTxt.innerText = "Getting weather details...";
+  infoTxt.classList.add("pending");
+
+  fetch(api)
+    .then((response) => response.json())
+    .then((result) => weatherDetails(result));
+}
+
+function weatherDetails(info) {
+  infoTxt.classList.replace("pending", "error");
+  if (info.cod == "404") {
+    infoTxt.innerText = `${inputField.value} isn't a valid city name`;
+  } else {
+    const city = info.name;
+    const country = info.sys.country;
+    const { description, id } = info.weather[0];
+    const { feels_like, humidity, temp } = info.main;
+
+    if (id == 800) {
+      wIcon.src = "images/weather-icons/clear.svg";
+    } else if (id >= 200 && id <= 232) {
+      wIcon.src = "images/weather-icons/strom.svg";
+    } else if (id >= 600 && id <= 622) {
+      wIcon.src = "images/weather-icons/snow.svg";
+    } else if (id >= 701 && id <= 781) {
+      wIcon.src = "images/weather-icons/haze.svg";
+    } else if (id >= 801 && id <= 804) {
+      wIcon.src = "images/weather-icons/cloud.svg";
+    } else if ((id >= 300 && id <= 321) || (id >= 500 && id <= 531)) {
+      wIcon.src = "images/weather-icons/rain.svg";
+    }
+
+    wrapper.querySelector(".temp .num").innerText = Math.floor(temp);
+    wrapper.querySelector(".weather").innerText = description;
+    wrapper.querySelector(".location span").innerText = `${city}, ${country}`;
+    wrapper.querySelector(".temp .num-2").innerText = Math.floor(feels_like);
+    wrapper.querySelector(".humidity span").innerText = `${humidity}%`;
+
+    infoTxt.classList.remove("pending", "error");
+    wrapper.classList.add("active");
+  }
+}
+
+arrowBack.addEventListener("click", () => {
+  wrapper.classList.remove("active");
+});
